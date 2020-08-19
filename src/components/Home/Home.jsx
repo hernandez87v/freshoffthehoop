b97bce6a70425a6b22b56baeac4f1067dea8561c
hernandez87v <hernandez87v@hotmail.com>
2020-08-19 11:32:15 -0700

    Navbar setup and styling

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Home.scss';
+
+// var sectionStyle = {
+//   top: '0',
+//   bottom: '0',
+//   position: 'absolute',
+//   width: '100%',
+//   height: '100%',
+//     backgroundImage: `url(${Background})`,
+//   backgroundPosition: 'center',
+//   backgroundSize: 'cover',
+//   backgroundRepeat: 'no-repeat',
+// };
+
+function Home() {
+  return (
+    <>
+      <div className="home">
+        {/* <section style={sectionStyle}> </section> */}
+      </div>
+    </>
+  );
+}
+
+export default Home;
