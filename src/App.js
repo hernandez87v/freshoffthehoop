8d0dae5dbc55afeae8c9ecc730c1f822b58de254
hernandez87v <hernandez87v@hotmail.com>
2020-08-19 12:06:48 -0700

    Add About page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.scss';
 import Home from './components/Home/Home';
-// import Gallery from './components/Gallery/Gallery';
-// import Soundcloud from './components/Soundcloud/Soundcloud';
-// import Mixcloud from './components/Mixcloud/Mixcloud';
+import About from './components/About/About';
 // import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 
@@ -23,9 +21,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
-          {/* <Route path="/Gallery" exact component={Gallery} />
-          <Route path="/Soundcloud" exact component={Soundcloud} />
-          <Route path="/Mixcloud" exact component={Mixcloud} /> */}
+          <Route path="/About" exact component={About} />
         </Switch>
         {/* <Footer /> */}
       </div>
---
+++ src/components/About/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './About.scss';
+
+function About() {
+  return (
+    <div className="about">
+      <h2>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod sed
+        corrupti, accusamus dolore dolores, quaerat beatae minus numquam
+        incidunt veniam, sint praesentium! Qui, cumque excepturi culpa
+        exercitationem cupiditate repellendus molestias!
+      </h2>
+    </div>
+  );
+}
+
+export default About;
