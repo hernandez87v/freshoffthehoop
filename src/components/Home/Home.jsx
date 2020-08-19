0b6980ea85e5c8c0d6e56a0c92d13594a5c91ff2
hernandez87v <hernandez87v@hotmail.com>
2020-08-19 12:29:27 -0700

    Add very knit background

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import './Home.scss';
+import Background from './knitting250px_@2X.png';
 
-// var sectionStyle = {
-//   top: '0',
-//   bottom: '0',
-//   position: 'absolute',
-//   width: '100%',
-//   height: '100%',
-//     backgroundImage: `url(${Background})`,
-//   backgroundPosition: 'center',
-//   backgroundSize: 'cover',
-//   backgroundRepeat: 'no-repeat',
-// };
+var sectionStyle = {
+  top: '0',
+  bottom: '0',
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+  backgroundImage: `url(${Background})`,
+  backgroundPosition: 'center',
+};
 
 function Home() {
   return (
     <>
       <div className="home">
-        {/* <section style={sectionStyle}> </section> */}
+        <section style={sectionStyle}> </section>
       </div>
     </>
   );
